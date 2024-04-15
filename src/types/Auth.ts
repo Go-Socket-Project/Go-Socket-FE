f761d4c3b71e42859ec25ffe6ff41698724c3bfb
Jeongmin <srm0609@naver.com>
2024-04-15 14:39:15 +0900

    Update: types
    SigninForm, SignupForm, AuthButtonProps

--- src/types/Auth.ts
+++ src/types/Auth.ts
@@ -10,3 +10,21 @@ export interface AuthInputProps
   labelName?: string;
   isValue?: boolean;
 }
+
+export interface SigninForm {
+  email?: string;
+  password?: string;
+}
+
+export interface SignupForm {
+  name?: string;
+  email?: string;
+  password?: string;
+  passwordCheck?: string;
+}
+
+export interface AuthButtonProps {
+  text: string;
+  type: "button" | "submit";
+  onClick: () => void;
+}
