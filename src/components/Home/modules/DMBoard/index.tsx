6b93360d7ca019f477209d0ec707cea972a134b4
Jeongmin <srm0609@naver.com>
2024-05-17 23:19:10 +0900

    Update: DMBoard

--- src/components/Home/modules/DMBoard/index.tsx
+++ src/components/Home/modules/DMBoard/index.tsx
@@ -1,11 +1,48 @@
 "use client";
 
 import * as S from "./style";
-
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import ChatInput from "@/components/Home/atoms/ChatInput";
 
 const DMBoard = () => {
-  const inbox = ["hi", "hello", "nice"];
+  const path = usePathname();
+  const id = path.split("/").pop();
+
+  const [socket, setSocket] = useState<any>(undefined);
+  const [inbox, setInbox] = useState<string[]>([]);
+  const [message, setMessage] = useState<string>("");
+
+  const handelSendMeassage = () => {
+    socket.send(JSON.stringify({ message: message }));
+    setMessage("");
+  };
+
+  useEffect(() => {
+    const socket = new WebSocket(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/ws/chat/${id}/`
+    );
+
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      setInbox((inbox: string[]) => [...inbox, data.message]);
+      console.log(data);
+    };
+
+    socket.onopen = () => {
+      socket.send(JSON.stringify({ message: "Hello, server!" }));
+    };
+
+    socket.onerror = (error) => {
+      console.error("WebSocket error observed:", error);
+    };
+
+    socket.onclose = (event) => {
+      console.log("WebSocket is closed now.", event);
+    };
+
+    setSocket(socket);
+  }, []);
 
   return (
     <S.DMBoardWrapper>
@@ -27,7 +64,11 @@ const DMBoard = () => {
         </div>
       </S.ChatBoard>
 
-      <ChatInput />
+      <ChatInput
+        handelSendMeassage={handelSendMeassage}
+        message={message}
+        setMessage={setMessage}
+      />
     </S.DMBoardWrapper>
   );
 };
