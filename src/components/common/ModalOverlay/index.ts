f11ede5dc35a464715473d6b14823c77584b58ca
Jeongmin <srm0609@naver.com>
2024-05-20 15:53:44 +0900

    Create: ModalOverlay

---
+++ src/components/common/ModalOverlay/index.ts
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const ModalOverlayWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: hsla(235, 77%, 10%, 0.45);
+`;
